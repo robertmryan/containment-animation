1869464306f21c425e30940dcd069e4c7b4e8304
Robert Ryan <rryan@Robert-Ryans-iMac.local>
2012-12-16 23:18:39 -0500

    Initial Commit

---
+++ Containment Animation Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Containment Animation Test
+//
+//  Created by Robert Ryan on 12/16/12.
+//  Copyright (c) 2012 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Containment Animation Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Containment Animation Test
+//
+//  Created by Robert Ryan on 12/16/12.
+//  Copyright (c) 2012 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
